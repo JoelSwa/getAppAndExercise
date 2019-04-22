57bb88b27a36dc7976629e9f947583ed17fca171
Joel <joel.swanelid@gmail.com>
2019-04-22 12:16:38 +0200

    Added simple first draft of login page. Rudimentary list from Ionic still present.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+/* tslint:disable */
+import {Component, Injectable} from '@angular/core';
+import {HttpClient, HttpRequest} from '@angular/common/http';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
 })
+@Injectable()
 export class HomePage {
 
+    constructor(private http: HttpClient) {
+    }
+
+    data: any;
+
+    public logIn() {
+        this.http.get('http://192.168.1.71:8080/users', {responseType: 'text'}).subscribe(
+            response => this.data = response.toString());
+        console.log('login');
+        if (this.data) {
+            alert(this.data);
+        } else {
+            alert('Data == null');
+        }
+    }
 }
